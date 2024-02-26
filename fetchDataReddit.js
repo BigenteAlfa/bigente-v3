5b21f1f758851243daa21a49f3cf11f869830ce2
BigenteAlfa <bigentealfa@gmail.com>
2024-02-25 21:29:29 -0300

    first commit

---
+++ fetchDataReddit.js
@@ -0,0 +1,37 @@
+async function fetchDataReddit(subreddits) {
+    const outputData = []
+    const fetch = await import('node-fetch');
+
+    for (const subreddit of subreddits) {
+        const url = `https://www.reddit.com/r/${subreddit}.json`;
+
+        try {
+            const response = await fetch.default(url);
+            const data = await response.json();
+
+            data.data.children.forEach(post => {
+                // Tomamos las variables que queremos rescatar de cada post
+                const { id, title, subreddit, selftext, thumbnail, score, url, created_utc } = post.data;
+
+                // Procesamos las variables antes de entregarlas
+                const sanitizedThumbnail = thumbnail === 'self' ? '' : thumbnail;
+                const time_created = new Date(created_utc * 1000);
+
+                // Configuramos la respuesta
+                outputData.push({ id, title, subreddit, selftext, sanitizedThumbnail, score, url, time_created })
+            });
+        } catch (error) {
+            console.error(`Error fetching data for subreddit ${subreddit}:`, error);
+        }
+    }
+
+    return outputData
+}
+
+module.exports = fetchDataReddit;
+
+// Test
+//const subreddits = ['RepublicadeChile', 'chile', 'Santiago'];
+//fetchDataReddit(subreddits)
+//  .then(data => console.log(JSON.stringify(data, null, 2)))
+//  .catch(error => console.error('Error en la prueba:', error));
---
+++ submit_reddit_chile.js
@@ -0,0 +1,63 @@
+const fetchDataReddit = require('./fetchDataReddit');
+const { Pool } = require('pg');
+
+// Creamos instancia de Pool usando la cadena de conexión a la base de datos ElephantSQL
+const pool = new Pool({
+    connectionString: 'postgres://wqjfzrfu:050l8qzRWMEkR10dI7CNfddmnZUJS0Tp@silly.db.elephantsql.com/wqjfzrfu',
+    ssl: {
+      rejectUnauthorized: false 
+    }
+});
+
+// Función para insertar datos de Reddit en la base de datos
+async function insertarDatosReddit(posts) {
+    const client = await pool.connect();
+    let nuevosPosts = 0;
+    let postsEditados = 0;
+
+    try {
+        await Promise.all(posts.map(async (post) => {
+            const { id, title, subreddit, selftext, thumbnail, score, url, time_created } = post;
+            const query = `
+                INSERT INTO db_reddit_chile (id, title, subreddit, selftext, thumbnail, score, url, time_created)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                ON CONFLICT (id) DO UPDATE
+                SET score = EXCLUDED.score, time_edited = CURRENT_TIMESTAMP AT TIME ZONE 'UTC' AT TIME ZONE '-03'
+                RETURNING time_edited; -- Retorna el valor de time_edited después de la actualización
+            `;
+            const values = [id, title, subreddit, selftext, thumbnail, score, url, time_created];
+            const result = await client.query(query, values);
+
+            if (result.rowCount === 1 && result.rows[0].time_edited) {
+                postsEditados++;
+            } else {
+                nuevosPosts++;
+            }
+        }));
+
+        console.log(`Datos de Reddit insertados correctamente. Nuevos: ${nuevosPosts}, Editados: ${postsEditados}`);
+    } catch (error) {
+        console.error('Error al insertar datos de Reddit:', error);
+    } finally {
+        client.release();
+    }
+}
+
+// Función principal que será invocada
+async function submitRedditChile() {
+    try {
+        // Obtener datos de Reddit
+        const subreddits = ['chile', 'RepublicadeChile', 'Santiago', 'ChileIT', 'Chilefit'];
+        const redditPosts = await fetchDataReddit(subreddits);
+
+        // Insertar los datos en la base de datos
+        await insertarDatosReddit(redditPosts);
+
+        console.log('Proceso completado exitosamente');
+    } catch (error) {
+        console.error('Error en el proceso:', error);
+    }
+}
+
+// Llama a la función principal para iniciar el proceso
+submitRedditChile();
